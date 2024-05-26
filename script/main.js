8f23e383bae141d848ccf6f7efcce25a761bb0c9
Manuel <manueldipilla3@gmail.com>
2024-05-26 23:06:48 +0200

    migliorata efficenza animazione entrata, aggiunte animazioni menu mobile e desktop

--- script/main.js
+++ script/main.js
@@ -8,21 +8,27 @@ jQuery(document).ready(function($) {
 
     setTimeout(function() { 
         $("#background").addClass("background-image puff-in-center");
-        setTimeout(function(){
-            $("#filigrana").addClass("filigrana-image slide-in-fwd-center");
-        }, 700)
-        $("#container-logo").addClass("move-up");
-        $("#home-desc").addClass("move-up-2");
+        setTimeout(() => {
+            setTimeout(function(){
+                $("#filigrana").addClass("filigrana-image slide-in-fwd-center");
+            }, 500)
+            $("#container-logo").addClass("move-up");
+            $("#home-desc").addClass("move-up-2");
+            $("#expanded-menu").removeClass("visibility-hidden").addClass("visibility-visible slide-in-top");
+            $('#small-menu').removeClass('display-none');
+        }, 200);
     }, 3000);
 
     $('#small-menu').click(function () {
-        $('#hidden-menu').removeClass('display-none');
+        $('#hidden-menu').removeClass('display-none scale-out-tl').addClass('scale-in-tl');
         $('#small-menu').addClass('display-none');
     });
 
     $('#close').click(function () {
-        $('#hidden-menu').addClass('display-none');
-        $('#small-menu').removeClass('display-none');
+        $('#hidden-menu').removeClass('scale-in-tl').addClass('scale-out-tl');
+        setTimeout(() => {
+            $('#small-menu').removeClass('display-none');
+        }, 400);
     });
 
  })
