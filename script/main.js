c6ccd77f448848543affb748c278c90476ab2f21
Manuel <manueldipilla3@gmail.com>
2024-05-27 18:52:28 +0200

    add animation on scroll and menu, added page services with card and animations

--- script/main.js
+++ script/main.js
@@ -1,21 +1,27 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide',
- 	once: false
- });
- 
 jQuery(document).ready(function($) {
 
     setTimeout(function() { 
         $("#background").addClass("background-image puff-in-center");
         setTimeout(() => {
+
             setTimeout(function(){
                 $("#filigrana").addClass("filigrana-image slide-in-fwd-center");
             }, 500)
+
             $("#container-logo").addClass("move-up");
             $("#home-desc").addClass("move-up-2");
             $("#expanded-menu").removeClass("visibility-hidden").addClass("visibility-visible slide-in-top");
             $('#small-menu').removeClass('display-none');
+
+            setTimeout(() => {
+                $('#main').removeClass('display-none');
+                AOS.init({
+                    duration: 800,
+                    easing: 'slide',
+                    once: false
+                });
+            }, 1200);
+
         }, 200);
     }, 3000);
 
---
+++ script/menu_scroll.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function ($) {
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
+            $('.navbar-style').addClass('manu-black');
+        } else {
+            $('.navbar-style').removeClass('manu-black');
+        }
+    });
+
+    var header = document.querySelector("header");
+    var links = document.querySelectorAll(".link");
+    var sections = document.querySelectorAll('.sections');
+    window.onscroll = function () {
+        var headerOffset = header.offsetHeight;
+        var current = 'home';
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            if (scrollY >= sectionTop - 60) {
+                current = section.getAttribute('id');
+            }
+        })
+        links.forEach((item) => {
+            item.classList.remove('active');
+            if (item.href.includes(current)) {
+                item.classList.add("active")
+            } else {
+                item.classList.remove("active")
+
+            }
+
+
+        })
+    }
+
+})
