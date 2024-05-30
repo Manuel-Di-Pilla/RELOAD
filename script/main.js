d4330dce7022203b888fa8622c5b1e3cd07c5fc1
Manuel <manueldipilla3@gmail.com>
2024-05-30 13:42:48 +0200

    fix block rotation

--- script/main.js
+++ script/main.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($) {
     
-    var myScreenOrientation = window.screen.orientation;
-
-    myScreenOrientation.lock("portrait");
+    $(window).bind("orientationchange", function () {
+        var orientation = window.orientation;
+        var new_orientation = (orientation) ? 0 : 180 + orientation;
+        $('body').css({
+            "-webkit-transform": "rotate(" + new_orientation + "deg)"
+        });
+    });
 
     setTimeout(function() { 
         $("#background").addClass("background-image puff-in-center");
